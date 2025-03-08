0991f5c3b3b9a3a0a6faf190c5abe09efc90cfca
cesardarizaleta <cesardarizaleta@gmail.com>
2025-03-08 14:30:54 -0400

    Init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.cesardarizaleta.gestor {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.cesardarizaleta.gestor to javafx.fxml;
+    exports com.cesardarizaleta.gestor;
+}
